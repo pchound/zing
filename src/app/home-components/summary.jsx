f78e2c66218daf8a53271e10fb4ac35a366d8db8
pchound <chaonerdguy@gmail.com>
2025-10-21 11:39:28 -0600

    scorpion

--- src/app/home-components/summary.jsx
+++ src/app/home-components/summary.jsx
@@ -7,13 +7,7 @@ const Summary = () => {
 
   return (
     <div className="py-16 px-6">
-            <h1 className="text-4xl font-bold text-center mb-8 text-black"
-            style={{
-            fontFamily: '"Anton", sans-serif',
-            fontWeight: 700
-        }}>
-       At Zing Pest,
-        </h1>
+
       <div className="max-w-7xl mx-auto grid sm:grid-cols-1 md:grid-cols-3 gap-8 items-center">
       {/* Text block */}
 
@@ -21,22 +15,77 @@ const Summary = () => {
 
 <div className="flex justify-center">
   <Image
-    src="/home-pest3.jpg"
+    src="/scorpion2.jpg"
     alt="Pest Control Man"
     width={1000}
     height={1000}
-    className="rounded-xl shadow-xl"
+    className=""
   />
 </div>
-        <div className="md:col-span-2">
-          <p className="text-lg text-black mb-4"
-          style={{
-            fontFamily: '"Quicksand", sans-serif',
-            fontWeight: 400
+    <div className="md:col-span-2">
+        <h1 className="text-5xl font-bold mb-20 text-black"
+            style={{
+            fontFamily: '"Anton", sans-serif',
+            fontWeight: 700
         }}>
-          We are dedicated to providing top-notch pest control services to ensure your home and business remain pest-free. With years of experience in the industry, our team of licensed professionals utilizes the latest techniques and eco-friendly solutions to effectively eliminate pests while prioritizing the safety of your family and pets.
-          </p>
-        </div>
+       As a family owned business, we offer:
+        </h1>
+
+         <h1 className="text-3xl font-bold mb-3 text-black"
+            style={{
+            fontFamily: '"Anton", sans-serif',
+            fontWeight: 700
+        }}>
+       Custom solutions
+        </h1>       
+
+        <p className="text-lg text-black mb-4"
+        style={{
+          fontFamily: '"Quicksand", sans-serif',
+          fontWeight: 400
+      }}>
+          We understand that every home and business is unique. That's why we tailor our pest control services to meet your specific needs, ensuring effective and lasting results.
+        </p>
+
+
+
+        <h1 className="text-3xl font-bold mb-3 text-black"
+            style={{
+            fontFamily: '"Anton", sans-serif',
+            fontWeight: 700
+        }}>
+       Long term protection
+        </h1>       
+
+        <p className="text-lg text-black mb-4"
+        style={{
+          fontFamily: '"Quicksand", sans-serif',
+          fontWeight: 400
+      }}>
+          Our goal is not just to eliminate pests but to prevent future infestations. We provide ongoing maintenance plans and advice to keep your property pest-free year-round.
+        </p>
+
+
+
+        <h1 className="text-3xl font-bold mb-3 text-black"
+            style={{
+            fontFamily: '"Anton", sans-serif',
+            fontWeight: 700
+        }}>
+       Fast response times
+        </h1>       
+
+        <p className="text-lg text-black mb-4"
+        style={{
+          fontFamily: '"Quicksand", sans-serif',
+          fontWeight: 400
+      }}>
+          We know that pest problems require immediate attention. Our team is committed to responding quickly to your service requests, minimizing the impact on your home or business.
+        </p>
+
+          
+
+    </div>
 
     </div>
     </div>
